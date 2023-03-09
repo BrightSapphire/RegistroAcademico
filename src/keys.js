fe8d3c6b419b1878f9f61288c0889e9682f709a5
BrightSapphire <63077488+BrightSapphire@users.noreply.github.com>
2023-03-08 22:13:49 -0500

    Add files via upload

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 60,
+        host: process.env.DB_host || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_DATABASE || 'db_registro'
+    }
+
+};
